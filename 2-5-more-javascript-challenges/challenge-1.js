1f58071b0298a7cd8cff8d2d79d01eb9df333460
MaratFa <evolution99@rambler.ru>
2024-04-02 08:24:52 +0300

    Renamed

--- 2-5-more-javascript-challenges/challenge-1.js
+++
@@ -1,7 +0,0 @@
-let numberVariable = 0;
-
-numberVariable++;
-numberVariable++;
-numberVariable++;
-
-console.log(numberVariable); // 3
--- 2-5-more-javascript-challenges/challenge-2.js
+++
@@ -1,8 +0,0 @@
-// The same result
-
-const firstNumber = 20;
-const secondNumber = "20";
-
-const result = firstNumber === secondNumber;
-
-console.log(result);
